2cc17dc83b6ac975a9251bd29c5b6a75b7028870
Nicolas Bauer <nicolaswilliam.bauer@gmail.com>
2016-01-21 20:43:59 -0800

    clicking on familyView changes graph, also summaryView listens for changes in familyData

--- client/summaryView/summary.js
+++ client/summaryView/summary.js
@@ -3,13 +3,34 @@ angular.module('gaussHyrax.summary',['SummaryServicesModule'])
 .controller('summaryCtrl',['$scope','SummaryFactory',function($scope,SummaryFactory){
   console.log('summary controller loaded');
 
+  $scope.who = "Everybody"
+  //computes data for the whole family on load
   //$scope.familyData is from the familyViewController
-
-  //just computes data for  the first family member
-  var familyPlot = SummaryFactory.calculatePoints($scope.familyData);
-  //create chart
-  console.log('familyPlot',familyPlot);
+  var familyPlot = SummaryFactory.calculateGraphForSetOfFamilyMembers($scope.familyData);
   SummaryFactory.makeChart(familyPlot);
+
+
+  //will change the plot to a single family member when the active member is clicked
+  //activeFamilyMember is set by familyController
+  $scope.$watch('activeFamilyMember',function(){
+    console.log('familyMember selected, changing graph...');
+    if($scope.activeFamilyMember){
+      var singlePlot = SummaryFactory.calculateGraphForOneFamilyMember($scope.activeFamilyMember['_id'])
+      SummaryFactory.makeChart(singlePlot);
+      $scope.who = $scope.activeFamilyMember.firstName + ' ' + $scope.activeFamilyMember.lastName 
+    }else{
+      console.log('cannot plot, family member not specified');
+    }
+  });
+
+  $scope.$watch('familyData',function(){
+    console.log('familyData changed, recomputing all graphs...');
+    if($scope.familyData){
+      SummaryFactory.calculateGraphForSetOfFamilyMembers($scope.familyData);
+    }else{
+      console.log('cannot plot, family not specified');
+    }
+  });
 
 
 }])
--- client/summaryView/summaryServices.js
+++ client/summaryView/summaryServices.js
@@ -1,14 +1,105 @@
 angular.module('SummaryServicesModule',[])
 
 .factory('SummaryFactory',['$http','$window',function($http,$window){
-  console.log('summary factory loaded');
-  var chart;
+
+  var dateFormat = "MMM D, YY";
+
   var factory = {};
-  var rendered;
+  factory.currentPointValue = {};
+  factory.pointGraph = {};
+  factory.xLabels = [];
+
+  //helper function
+  var makeSeriesForOneFamilyMember = function(familyMember,dayIdx){
+    var points=[];
+    var lastPoints = 0;
+    var numDays = Object.keys(dayIdx).length;
+
+    //initialize points array to be 0
+    for (var i = 0; i < numDays; i++) {
+      points[i] = 0;
+    }
+
+    //add points that are earned
+    for (var i = 0; i < familyMember.history.length; i++) {
+      points[dayIdx[moment(familyMember.history[i].date).format(dateFormat)]]+=familyMember.history[i].points;
+    }
+
+    //interpolate
+    for (var i = 0; i < numDays; i++) {
+
+      if(points[i] === 0){
+        //interpolate
+        if(lastPoints > 1){
+          points[i] = lastPoints - 1
+        }else{
+          points[i]=0
+        }
+      }else{
+        //add points to earned value
+        points[i] += lastPoints;
+      }
+
+      //update the previous point value
+      lastPoints = points[i];
+    }
+
+    return _.flatten([familyMember.firstName + ' ' + familyMember.lastName,points]);
+  }
+
+
+  factory.calculateGraphForSetOfFamilyMembers = function(family){
+    //min date will be the first history item of the first person added
+    var minDate = family[0].history[0].date;
+    var day = moment(minDate);
+    var now = moment();
+    var series;
+
+    var days = [];
+    var dayIdx = {};
+
+    
+    //determine x values
+    var i = 0;
+    while(day < now){
+
+      days.push(day.format(dateFormat));
+      dayIdx[day.format(dateFormat)] = i;
+      day.add(1,'days');
+      i++;
+    }
+    //store this so it can be used later
+    this.xLabels = _.flatten(['x',days]);
+
+    //calculate series for each family member
+    var points = [];
+    for (var i = 0; i < family.length; i++) {
+      series  = makeSeriesForOneFamilyMember(family[i],dayIdx,dateFormat)
+      points.push(series.slice());
+      
+      //store this data so it can be retrieved/updated later
+      this.pointGraph[family[i]['_id']] = series.slice();
+      this.currentPointValue[family[i]['_id']] = series[series.length-1];
+
+    };
+
+    //return data in special format for c3
+    points.unshift(factory.xLabels);
+    
+    return points;
+  };
+
+  factory.calculateGraphForOneFamilyMember = function(familyMemberId){
+    var output = [this.pointGraph[familyMemberId].slice()];
+    output.unshift(this.xLabels);
+    return output;
+  };
+
   factory.makeChart = function(data){
     var xAxis;
+    var rendered;
 
-    chart = c3.generate({
+    var chart = c3.generate({
       
       bindto: '#chart',
       data:{
@@ -54,75 +145,6 @@ angular.module('SummaryServicesModule',[])
     });
  
   };
-
-  var makeSeriesForOneFamilyMember = function(familyMember,dayIdx, dateFormat){
-    var points=[];
-    var lastPoints = 0;
-    var numDays = Object.keys(dayIdx).length;
-    //initialize points array to be 0
-    for (var i = 0; i < numDays; i++) {
-      points[i] = 0;
-    }
-
-    //add points
-    for (var i = 0; i < familyMember.history.length; i++) {
-      points[dayIdx[moment(familyMember.history[i].date).format(dateFormat)]]+=familyMember.history[i].points;
-    }
-
-    //interpolation
-    for (var i = 0; i < numDays; i++) {
-
-      if(points[i] === 0){
-        //interpolate
-        if(lastPoints > 1){
-          points[i] = lastPoints - 1
-        }else{
-          points[i]=0
-        }
-      }else{
-        //add points to earned value
-        points[i] += lastPoints;
-      }
-
-      //update the previous point value
-      lastPoints = points[i];
-    }
-
-    return _.flatten([familyMember.firstName + ' ' + familyMember.lastName,points]);
-  }
-
-
-  factory.calculatePoints = function(family){
-    var minDate = family[0].history[0].date;
-    var day = moment(minDate);
-    var now = moment();
-    var output;
-
-    var days = [];
-    var dayIdx = {};
-
-    var dateFormat = "MMM D, YY";
-    var i = 0;
-    while(day < now){
-
-      days.push(day.format(dateFormat));
-      dayIdx[day.format(dateFormat)] = i;
-      day.add(1,'days');
-      i++;
-    }
-
-    var points = []
-
-    for (var i = 0; i < family.length; i++) {
-      points.push(makeSeriesForOneFamilyMember(family[i],dayIdx,dateFormat));
-    };
-
-    //return data in special format for c3
-    points.unshift(_.flatten(['x',days]));
-    
-    return points;
-  };
-
   return factory;
 
 }])
